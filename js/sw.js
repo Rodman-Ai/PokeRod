c6ddabc27c1ff0e4df2501c544a70c81e410c62a
Leonard Rodman <rodman-ai@users.noreply.github.com>
2026-05-06 00:36:11 -0500

    Add atlas editor grid borders

--- js/sw.js
+++ js/sw.js
@@ -1,7 +1,7 @@
 // PokeRod service worker - caches the app shell for offline play.
 'use strict';
 
-const CACHE = 'pokerod-shell-v40';
+const CACHE = 'pokerod-shell-v41';
 const ASSETS = [
   './',
   './index.html',
